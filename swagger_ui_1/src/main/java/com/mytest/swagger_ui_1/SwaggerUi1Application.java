e16d4d0878ee71778d9b7464cc155ee89d5d52cb
DESKTOP-0C3MHUG\cky <kycheung1985@gmail.com>
2022-10-01 21:14:51 +0800

    init

---
+++ swagger_ui_1/src/main/java/com/mytest/swagger_ui_1/SwaggerUi1Application.java
@@ -0,0 +1,13 @@
+package com.mytest.swagger_ui_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerUi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerUi1Application.class, args);
+	}
+
+}
---
+++ swagger_ui_1/src/test/java/com/mytest/swagger_ui_1/SwaggerUi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytest.swagger_ui_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerUi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
