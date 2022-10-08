f9d10d2d2136b3ed4a79eda6907031de76654685
DESKTOP-0C3MHUG\cky <kycheung1985@gmail.com>
2022-10-08 23:08:30 +0800

    Add discovery client

---
+++ discovery_client/src/main/java/com/mytest/rest/webservices/discovery_client/DiscoveryClientApplication.java
@@ -0,0 +1,26 @@
+package com.mytest.rest.webservices.discovery_client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+
+import com.netflix.discovery.EurekaClient;
+
+@SpringBootApplication
+public class DiscoveryClientApplication {
+	
+	@Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryClientApplication.class, args);
+	}
+
+}
---
+++ discovery_client/src/test/java/com/mytest/rest/webservices/discovery_client/DiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytest.rest.webservices.discovery_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
